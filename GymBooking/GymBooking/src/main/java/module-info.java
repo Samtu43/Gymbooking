bd5de8c39892955581f514b668f998795ea589b0
Sam Mariscal <sammiermariscal@gmail.com>
2025-11-30 00:49:29 +0800

    Delete GymBooking/GymBooking directory

--- GymBooking/GymBooking/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.gymbooking {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.gymbooking to javafx.fxml;
-    exports com.example.gymbooking;
-}
